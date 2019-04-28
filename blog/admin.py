bf473a2f06ae63918aa89c1ed0b5a51f5f5e4915
stool <christry.stool@gmail.com>
2019-04-29 01:21:51 +0800

    admin version2

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.admin.models import LogEntry
 from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
@@ -8,7 +9,9 @@ from django.utils.html import format_html
 from .adminforms import PostAdminForm
 from .models import Post, Category, Tag
 
+from typeidea.base_admin import BaseOwnerAdmin
 from typeidea.custom_site import custom_site
+
 
 # Register your models here.
 
@@ -18,25 +21,22 @@ class PostInline(admin.TabularInline):
     model = Post
 
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+@admin.register(Category, site=custom_site)
+class CategoryAdmin(BaseOwnerAdmin):
     inlines = [PostInline, ]
-    list_display = ('name', 'status', 'is_nav', 'created_time')
+    list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count')
     fields = ('name', 'status', 'is_nav')
 
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(CategoryAdmin, self).save_model(request, obj, form, change)
+    def post_count(self, obj):
+        return obj.post_set.count()
+
+    post_count.short_description = '文章数量'
 
 
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
+@admin.register(Tag, site=custom_site)
+class TagAdmin(BaseOwnerAdmin):
     list_display = ('name', 'status', 'created_time')
     fields = ('name', 'status')
-
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(TagAdmin, self).save_model(request, obj, form, change)
 
 
 class CategoryOwnerFilter(admin.SimpleListFilter):
@@ -56,7 +56,7 @@ class CategoryOwnerFilter(admin.SimpleListFilter):
 
 
 @admin.register(Post, site=custom_site)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(BaseOwnerAdmin):
     form = PostAdminForm
     list_display = [
         'title', 'category', 'status',
@@ -75,6 +75,7 @@ class PostAdmin(admin.ModelAdmin):
 
     exclude = ('owner',)
 
+    """
     fields = (
         ('category', 'title'),
         'desc',
@@ -82,6 +83,29 @@ class PostAdmin(admin.ModelAdmin):
         'content',
         'tag',
     )
+    """
+
+    fieldsets = (
+        ('基础配置', {
+            'description': '基础配置描述',
+            'fields': (
+                ('title', 'category'),
+                'status',
+            ),
+        }),
+        ('内容', {
+            'fields': (
+                'desc',
+                'content',
+            )
+        }),
+        ('额外信息', {
+            'classes': ('wide', ),
+            'fields': ('tag', ),
+        })
+    )
+
+    filter_vertical = ('tag', )
 
     def operator(self, obj):
         return format_html(
@@ -91,10 +115,7 @@ class PostAdmin(admin.ModelAdmin):
 
     operator.short_description = '操作'
 
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(PostAdmin, self).save_model(request, obj, form, change)
 
-    def get_queryset(self, request):
-        qs = super(PostAdmin, self).get_queryset(request)
-        return qs.filter(owner=request.user)
+@admin.register(LogEntry, site=custom_site)
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ['object_repr', 'object_id', 'action_flag', 'user', 'change_message']
