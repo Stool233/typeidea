e73450598e0eb696d3adddf29646cdc2505d406a
stool <christry.stool@gmail.com>
2019-04-28 23:38:32 +0800

    app installed

--- typeidea/wsgi.py
+++ typeidea/wsgi.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 
-#from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "typeidea.settings")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "typeidea.settings")
 profile = os.environ.get('TYPEIDEA_PROFILE', 'develop')
-os.environ.setdefault("DEJANGO_SETTINGS_MODULE", "typeidea.settings.%s" % profile)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "typeidea.settings.%s" % profile)
 
 application = get_wsgi_application()
